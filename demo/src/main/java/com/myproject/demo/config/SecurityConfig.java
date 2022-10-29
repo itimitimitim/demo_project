8a036d09c042acbb005650346a008a5fc485f5e4
patta <patta@host.docker.internal>
2022-10-29 23:30:28 +0700

    update cores config

--- demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	    @Bean
