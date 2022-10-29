fcc2ed2286a54c906747c64d71730ddc53a07504
patta <patta@host.docker.internal>
2022-10-29 23:29:17 +0700

    update cors config

--- demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
 @Configuration
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	    @Bean
@@ -21,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	        origins.add("http://localhost:9999");
 	        origins.add("http://localhost:8082");
 	        origins.add("http://localhost:8081");
+	        origins.add("http://localhost:3000");
 	        
 	        CorsConfiguration configuration = new CorsConfiguration();
 	        configuration.setAllowCredentials(true);
--- demo/src/main/java/com/myproject/demo/controller/StockController.java
+++ demo/src/main/java/com/myproject/demo/controller/StockController.java
@@ -3,9 +3,11 @@ package com.myproject.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,8 @@ import com.myproject.demo.repositories.StockRepositories;
 import com.myproject.demo.service.StockService;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value = "/api/v1")
 public class StockController {
 	
 	@Autowired
@@ -21,8 +25,6 @@ public class StockController {
 	
 	@Autowired
 	private StockRepositories stockRepository;
-	
-	
 
 	@GetMapping("/items")
 	public List<StockEntity> items(){
