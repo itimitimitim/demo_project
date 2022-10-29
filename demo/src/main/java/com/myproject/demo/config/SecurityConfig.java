f26d2e41a25ca0d51ab70766fc7a66725206a05b
patta <patta@host.docker.internal>
2022-10-29 14:54:19 +0700

    add security

--- demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/myproject/demo/config/SecurityConfig.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	    @Bean
 	    public CorsConfigurationSource corsConfigurationSource() {
